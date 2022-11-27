36c423885a766f00fbc687bae7eeb1bf74be96fe
HMit19 <maivanhieu19052002@gmail.com>
2022-11-27 14:10:31 +0700

    config security

---
+++ src/main/java/com/codegym/appmanagersale/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.codegym.appmanagersale.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().loginPage(("/login"))
+                .usernameParameter("username")
+                .passwordParameter("password");
+        http.formLogin().defaultSuccessUrl("/home").failureUrl("/login?error");
+
+        http.logout().logoutSuccessUrl("/login");
+
+        http.exceptionHandling().accessDeniedPage("/access-denied");
+
+        http.authorizeRequests().antMatchers("/login", "/logout", "/home", "/register", "/store").permitAll()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/user/**").access("hasRole('ROLE_USER')")
+                .anyRequest().authenticated();
+        http.csrf().disable();
+    }
+}
