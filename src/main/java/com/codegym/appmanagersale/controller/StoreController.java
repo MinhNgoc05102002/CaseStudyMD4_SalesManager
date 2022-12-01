6095f1143a7aa2b06a32d3c8c5d1e1ebe143c804
Ngoc <ngoc43552@gmail.com>
2022-12-01 09:22:25 +0700

    show product demo in home page

--- src/main/java/com/codegym/appmanagersale/controller/StoreController.java
+++ src/main/java/com/codegym/appmanagersale/controller/StoreController.java
@@ -3,6 +3,7 @@ package com.codegym.appmanagersale.controller;
 import com.codegym.appmanagersale.service.product.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,10 +13,19 @@ public class StoreController {
 
     @Autowired
     private IProductService productService;
-    @RequestMapping("")
+    @GetMapping("")
     public ModelAndView showStore() {
         ModelAndView modelAndView = new ModelAndView("/user/store");
         modelAndView.addObject("products", productService.findAll());
         return modelAndView;
     }
+
+    @GetMapping("/showAllProducts")
+    public ModelAndView showAllProducts() {
+        ModelAndView modelAndView = new ModelAndView("user/productByCategory");
+        modelAndView.addObject("products", productService.findAll());
+        return modelAndView;
+    }
+
+//    @GetMapping()
 }
