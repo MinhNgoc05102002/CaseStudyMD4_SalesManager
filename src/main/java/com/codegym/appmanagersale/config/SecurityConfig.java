7f62471bd5b1bc25516bfd612ea6636b493beb99
Case <=>
2022-12-01 22:29:11 +0700

    update account service

--- src/main/java/com/codegym/appmanagersale/config/SecurityConfig.java
+++ src/main/java/com/codegym/appmanagersale/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.codegym.appmanagersale.config;
 
+import com.codegym.appmanagersale.service.account.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +8,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.security.Principal;
 
 @EnableWebSecurity
 @Configuration
@@ -32,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin().loginPage(("/login"))
                 .usernameParameter("username")
                 .passwordParameter("password");
+
         http.formLogin().defaultSuccessUrl("/store").failureUrl("/login?error");
 
         http.logout().logoutSuccessUrl("/login");
