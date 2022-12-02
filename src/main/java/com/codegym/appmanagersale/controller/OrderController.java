cef124b0c664f2fdbc53a1de904e068945016a4c
HieuMai <maivanhieu19052002@gmail.com>
2022-12-02 14:25:03 +0700

    show order at admin

--- src/main/java/com/codegym/appmanagersale/controller/OrderController.java
+++ src/main/java/com/codegym/appmanagersale/controller/OrderController.java
@@ -1,8 +1,23 @@
 package com.codegym.appmanagersale.controller;
 
+import com.codegym.appmanagersale.service.order.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/orders")
 public class OrderController {
 
+    @Autowired
+    private IOrderService orderService;
+
+    @GetMapping("")
+    public ModelAndView showOrder() {
+        ModelAndView modelAndView = new ModelAndView("/admin/order/list");
+        modelAndView.addObject("orders", orderService.findAll());
+        return modelAndView;
+    }
 }
--- src/main/java/com/codegym/appmanagersale/service/order/OrderService.java
+++ src/main/java/com/codegym/appmanagersale/service/order/OrderService.java
@@ -4,10 +4,12 @@ import com.codegym.appmanagersale.model.Category;
 import com.codegym.appmanagersale.model.Order;
 import com.codegym.appmanagersale.repository.IOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class OrderService implements IOrderService {
 
     @Autowired
