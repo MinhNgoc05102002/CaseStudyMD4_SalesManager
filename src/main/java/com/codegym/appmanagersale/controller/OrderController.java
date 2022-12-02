a490c13bb301504d319f34a52e5239be4b0b8b84
HieuMai <maivanhieu19052002@gmail.com>
2022-12-02 14:38:24 +0700

    show order

--- src/main/java/com/codegym/appmanagersale/controller/OrderController.java
+++ src/main/java/com/codegym/appmanagersale/controller/OrderController.java
@@ -1,15 +1,22 @@
 package com.codegym.appmanagersale.controller;
 
+import com.codegym.appmanagersale.model.Account;
+import com.codegym.appmanagersale.service.account.IAccountService;
 import com.codegym.appmanagersale.service.order.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
+    @Autowired
+    private IAccountService accountService;
 
     @Autowired
     private IOrderService orderService;
@@ -18,6 +25,14 @@ public class OrderController {
     public ModelAndView showOrder() {
         ModelAndView modelAndView = new ModelAndView("/admin/order/list");
         modelAndView.addObject("orders", orderService.findAll());
+        Account account = getUserCurrent();
+        modelAndView.addObject("account", account);
         return modelAndView;
+    }
+
+    public Account getUserCurrent() {
+        Principal principal = SecurityContextHolder.getContext().getAuthentication();
+        String username = principal.getName();
+        return accountService.findByUsername(username);
     }
 }
