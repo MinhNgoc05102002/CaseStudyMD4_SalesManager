a01bf7bda7c807b5f51a7321927afebd5ea84392
HieuMai <maivanhieu19052002@gmail.com>
2022-12-02 08:41:29 +0700

    show author

--- src/main/java/com/codegym/appmanagersale/controller/ProductController.java
+++ src/main/java/com/codegym/appmanagersale/controller/ProductController.java
@@ -75,12 +75,12 @@ public class ProductController {
         return "redirect:/products";
     }
 
-    @GetMapping("/edit/{id}")
-    public ModelAndView showEditForm(@PathVariable int id) {
-        ModelAndView modelAndView = new ModelAndView("/admin/product/edit");
-        modelAndView.addObject("product", productService.findById((long) id));
-        return modelAndView;
-    }
+//    @GetMapping("/edit/{id}")
+//    public ModelAndView showEditForm(@PathVariable int id) {
+//        ModelAndView modelAndView = new ModelAndView("/admin/product/edit");
+//        modelAndView.addObject("product", productService.findById((long) id));
+//        return modelAndView;
+//    }
 
     @PostMapping("/update")
     public String update(Product product, RedirectAttributes redirect) {
